6e065151051b1016d3fc6e4bc28104f6f97a8b86
serjalexle <serjalexle@gmail.com>
2022-02-21 12:54:56 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,36 @@
-import React, { useState } from 'react'
-import {JitsiMeeting} from '@jitsi/web-sdk'
+import React, { useEffect, useState } from 'react'
+import {Jutsu} from 'react-jutsu'
+import Head from 'next/head'
 
 const Index = () => {
+  const [room, setRoom] = useState('')
+  const [name, setName] = useState('')
+  const [call, setCall] = useState(false)
+
+  const handleClick = event => {
+    event.preventDefault()
+    if (room && name) setCall(true)
+  }
+
   return (
     <>
-      <h2>My First Meeting!</h2>
-      <JitsiMeeting
-    domain="localhost:3000"
-/>
+      <Head dangerouslySetInnerHTML={{ __html: "<script src='https://meet.jit.si/external_api.js'></script>" }} />
+      {
+        call ? (
+          <Jutsu
+            roomName={room}
+            userName={name}
+            loadingComponent={<p>loading ...</p>} />
+        ) : (
+          <form>
+            <input id='room' type='text' placeholder='Room' value={room} onChange={(e) => setRoom(e.target.value)} />
+            <input id='name' type='text' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
+            <button onClick={handleClick} type='submit'>
+              Start / Join
+            </button>
+          </form>
+        )
+      }
     </>
   )
 }
